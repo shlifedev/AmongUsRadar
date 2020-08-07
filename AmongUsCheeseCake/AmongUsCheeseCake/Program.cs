e181ab5f1be9cf15fbe8fecbd132dbaa9cdd26c1
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-08-07 16:48:20 +0900

    어몽어스 ESP 포지션

--- AmongUsCheeseCake/AmongUsCheeseCake/Program.cs
+++ AmongUsCheeseCake/AmongUsCheeseCake/Program.cs
@@ -25,7 +25,7 @@ namespace AmongUsCheeseCake
         };
         static void Main(string[] args)
         { 
-            string FIRST_PLAYER_OFFSET = "06854D20";
+            string FIRST_PLAYER_OFFSET = "06A94D20";
 
             Console.WriteLine("----포인터 디버거---");
             Console.WriteLine("주소A\t주소B\t계산결과");
@@ -45,16 +45,18 @@ namespace AmongUsCheeseCake
                 {
                     if (m.OpenProcess("Among us"))
                     {
+                        //플레이어 정보 가져오기
                         var size = S_PlayerControll.SizeOf();
                         var data = m.ReadBytes($"{FIRST_PLAYER_OFFSET}", size);
-                        var info = S_PlayerControll.FromBytes(data);
-              
+                        var info = S_PlayerControll.FromBytes(data); 
                         Console.WriteLine($"{info.PlayerId}\t{info.inVent}\t{info.killTimer}\t{info.NetTransform}");
 
-                        var vec2Hex = (m.ReadUIntPtr(info.NetTransform)+68).ToString("X");
-                        var vec2Data= m.ReadBytes($"{vec2Hex}", 8);
+        
+                        //플레이어의 netTransform 주소값
+                        var netTransform = ((int)info.NetTransform + 80).ToString("X"); 
+                        var vec2Data= m.ReadBytes($"{netTransform}", 8); // 주소로부터 8바이트 읽는다
                         S_Vector2 vec2 = S_Vector2.FromBytes(vec2Data);
-                        Console.WriteLine($"\t\t\tㄴ{vec2.x},{vec2.y}");
+                        Console.WriteLine($"\t\t\tㄴ{netTransform} {vec2.x},{vec2.y}");
                     }
                     System.Threading.Thread.Sleep(100);
                 }
