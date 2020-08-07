d549ddfba96faff328a73d8b77d271a738b54148
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-08-07 11:21:27 +0900

    x

---
+++ AmongUsCheeseCake/AmongUsCheeseCake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Memory;
+namespace AmongUsCheeseCake
+{
+    class Program
+    {
+        /*Memory Ref https://github.com/shlifedev/memory.dll*/
+        static public Mem m = new Mem(); 
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                if (m.OpenProcess("Among us"))
+                { 
+                    var x = m.AoBScan("?? ?? ?? 0A ?? ?? 0F");
+                    x.Wait();
+                    var result = x.Result;
+
+                    foreach(var m in result)
+                    {
+                        Console.WriteLine(result.Count());
+                    }
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
