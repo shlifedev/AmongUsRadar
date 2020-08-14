dcee3d77fc1045a894d57e3a2b6f9fe3a89c9542
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-08-14 15:37:38 +0900

    Update Utilis.cs

--- AmongUsCheeseCake/AmongUsCheeseCake/Cheat/Utilis.cs
+++ AmongUsCheeseCake/AmongUsCheeseCake/Cheat/Utilis.cs
@@ -12,7 +12,13 @@ namespace AmongUsCheeseCake.Cheat
     {
         public static string GetAddress(this long value) { return value.ToString("X"); }
         public static string GetAddress(this int value) { return value.ToString("X"); }
-        public static string GetAddress(this uint value) { return value.ToString("X"); }
-
+        public static string GetAddress(this uint value) { return value.ToString("X"); } 
+        public static string GetAddress(this IntPtr value) { return value.ToInt32().GetAddress(); }
+        public static IntPtr Sum(this IntPtr ptr, IntPtr ptr2) { return (IntPtr)(ptr.ToInt32() + ptr2.ToInt32()); }
+        public static IntPtr Sum(this IntPtr ptr, UIntPtr ptr2) { return (IntPtr)(ptr.ToInt32() + (int)ptr2.ToUInt32()); }
+        public static IntPtr Sum(this UIntPtr ptr, IntPtr ptr2) { return (IntPtr)(ptr.ToUInt32() + ptr2.ToInt32()); }
+        public static IntPtr Sum(this int ptr, IntPtr ptr2) { return (IntPtr)(ptr + ptr2.ToInt32()); }
+        public static IntPtr Sum(this IntPtr ptr, int ptr2) { return (IntPtr)(ptr.ToInt32() + ptr2); }
+ 
     }
 }
